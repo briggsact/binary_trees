6eafce69f85c100fdcfbe14717160268ef11211d
* <briggsact@gmail.com>
2022-08-25 21:15:18 +0100

    my commit

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is root - check node root or not
+ * binary_tree_is_root - checks if a node is a root
  * @node: node to check
- * Return: 1 if node if root 0 if not
+ *
+ * Return: 1 if node is a root
+ *         0 if not a root
+ *         0 if node is NULL
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-	else if (node->parent == NULL)
-		return (1);
-	return (0);
+	return ((!node || node->parent) ? 0 : 1);
 }
