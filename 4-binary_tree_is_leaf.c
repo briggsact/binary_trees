923a656ce35713b4e8f27a85220578c5d9a209fe
* <briggsact@gmail.com>
2022-08-25 21:11:47 +0100

    my commit

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is leaf - check node leaf or not
+ * binary_tree_is_leaf - checks if a node is a leaf
  * @node: node to check
- * Return: 1 if node if leaf 0 if not
+ *
+ * Return: 1 if node is a leaf
+ *         0 if not a leaf
+ *         0 if node is NULL
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-	else if (node->left == NULL && node->right == NULL)
-		return (1);
-	return (0);
+	return ((!node || node->left || node->right) ? 0 : 1);
 }
